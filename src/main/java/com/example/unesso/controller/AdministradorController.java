4718e15e9cc03ad45527d05852ae93c25af149a1
LuisPerezcx <luisperez90377@gmail.com>
2024-06-11 21:05:26 -0600

    paginas ligadas

--- src/main/java/com/example/unesso/controller/AdministradorController.java
+++ src/main/java/com/example/unesso/controller/AdministradorController.java
@@ -2,12 +2,32 @@ package com.example.unesso.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 
+@RequestMapping("/administrador")
 public class AdministradorController {
-    @GetMapping("/administrador")
-    public String administrador() {
+
+    @GetMapping("/menuAdministrador")
+    public String menuAdministrador() {
         return "/menuAdministrador";
+    }
+
+    @GetMapping("/estudiosSocieconomicos")
+    public String todasLasSolicitudes(){
+        return "/todasSolicitudes";
+    }
+    @GetMapping("/solicitudesRevisadas")
+    public String solicitudesRevisadas(){
+        return "/solicitudesRevisadas";
+    }
+    @GetMapping("/alumnos")
+    public String alumnos(){
+        return "/administrarAlumno";
+    }
+    @GetMapping("/fechas")
+    public String fechas(){
+        return "/administrarFecha";
     }
 }
