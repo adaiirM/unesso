701c79e1c5d0b732ef29524383334b4af1ce682b
LuisPerezcx <luisperez90377@gmail.com>
2024-06-11 18:41:00 -0600

    administrador controller

---
+++ src/main/java/com/example/unesso/controller/AdministradorController.java
@@ -0,0 +1,13 @@
+package com.example.unesso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class AdministradorController {
+    @GetMapping("/administrador")
+    public String administrador() {
+        return "/menuAdministrador";
+    }
+}
